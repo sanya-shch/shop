189f1bd7b592b6465189ad9d23a65eb4fd0bed0f
sanya-shch <shchipkov.sanya@ukr.net>
2019-11-09 19:42:34 +0200

    fix total price reducer

--- src/reducers/totalPriceReducer.js
+++ src/reducers/totalPriceReducer.js
@@ -11,7 +11,8 @@ export default (state = initialState, action) => {
         case ADD_TO_TOTAL_PRICE:
             return state === 0 ? state = action.payload : state = `${state.slice(0, -1)*1 + action.payload.slice(0, -1)*1}${action.payload.slice(-1)}`;
         case REMOVE_FROM_TOTAL_PRICE:
-            return action.payload.slice(0, -1)*1 === 0  ?  state = 0 : state = `${state.slice(0, -1)*1 - action.payload.slice(0, -1)*1}${action.payload.slice(-1)}`;
+            const tp = state.slice(0, -1)*1 - action.payload.slice(0, -1)*1;
+            return tp === 0  ?  state = 0 : state = `${tp}${action.payload.slice(-1)}`;
         case CLEAN_TOTAL_PRICE:
             return 0;
         default:
